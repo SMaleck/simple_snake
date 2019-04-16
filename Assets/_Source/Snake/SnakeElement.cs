4c8050c9fb69274035a0813669553584e3d55c80
Sebastian Maleck <s.maleck@outlook.de>
2019-04-16 18:21:09 +0200

    Adds Snake Element

---
+++ Assets/_Source/Snake/SnakeElement.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets._Source.Snake
+{
+    public class SnakeElement : MonoBehaviour
+    {
+        private SnakeHead _snakeHead;
+        public bool IsHead { get; private set; }
+
+        public void SetIsHead(bool value)
+        {
+            IsHead = value;
+        }
+
+        public void OnTriggerEnter2D(Collider2D other)
+        {
+            
+        }
+    }
+}
---
+++ Assets/_Source/Snake/SnakeManager.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets._Source.Snake
+{
+    public class SnakeManager : MonoBehaviour
+    {
+        [SerializeField] private SnakeElement _snakeElementPrefab;
+
+        private List<SnakeElement> _snakeElements;
+
+        public void OnPickedUpFood()
+        {
+
+        }
+
+        public void OnCollidedWithSelf()
+        {
+
+        }
+
+        private SnakeElement SpawnAt(Vector3 spawnPosition)
+        {
+            var snakeElement = Instantiate(_snakeElementPrefab);
+            snakeElement.gameObject.transform.position = spawnPosition;
+            
+            return snakeElement;
+        }
+
+        private void MoveElement()
+        {
+            var tail = _snakeElements.Last();
+        }
+    }
+}
